eca054a1502fc23284392f4cb0922b9c8fb88cd3
paul10510 <creeganpaul@gmail.com>
2022-11-03 09:32:29 -0400

    gfgjkdjngjkerngejkrng

--- app.py
+++ app.py
@@ -2,5 +2,8 @@ from flask import Flask, request
 app = Flask(__name__)
 @app.route('/recieve-request', methods=['POST'])
 def result():
-    print(request.form['foo']) # should display 'bar'
+    file = open("database.txt",  'a')
+    file.write(request.get_json.get("printRequest"))
     return 'Received !' # response to your request.
+
+app.run()
